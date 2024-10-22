b359bf101b2b492c0f68cb35fd255432cfc79fa5
Frutilupy22 <camiladiaz_0322@hotmail.com>
2024-10-22 06:04:59 -0500

    .

--- src/main/java/pe/edu/cibertec/ef_app_diaz_server/service/UsuarioService.java
+++ src/main/java/pe/edu/cibertec/ef_app_diaz_server/service/UsuarioService.java
@@ -1,8 +1,6 @@
 package pe.edu.cibertec.ef_app_diaz_server.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,9 +8,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import pe.edu.cibertec.ef_app_diaz_server.entity.Usuario;
 import pe.edu.cibertec.ef_app_diaz_server.repository.IUsuarioRepository;
-
-import java.util.Collection;
-import java.util.Collections;
 
 @Service
 public class UsuarioService implements UserDetailsService {
